06d6db6d978afdb43deaec9ab63aa08cdc39a8e6
Jiakang Chen <jiakangchen@Jiakangs-Macbook-Air-2020.local>
2020-09-20 00:21:44 -0700

    fixed some styling/made more responsive

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ var tl = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 
 tl.from(".animate1", 1, {x:500, opacity:0});
-tl.from(".animate2", 1, {x:-500, opacity:0}, "-=0.7");
+tl.from(".animate2", 1, {x:-500, opacity:0}, "+=0.3");
 
 const scene = new ScrollMagic.Scene({
     triggerElement: "#start-scrolling"
